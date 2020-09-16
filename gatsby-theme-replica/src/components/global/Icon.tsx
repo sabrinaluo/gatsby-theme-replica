00ce757f729341a8950f1f5655d2dcfaef800738
Sabrina Luo <luohaojing@gmail.com>
2020-09-17 02:28:08 +0800

    chore: remove unused files, add global css

--- gatsby-theme-replica/src/components/global/Icon.tsx
+++
@@ -1,8 +0,0 @@
-import MdiIcon from '@mdi/react';
-import React, { FC, memo } from 'react';
-
-const Icon: FC<any> = ({ path, size, ...props }) => {
-  return <MdiIcon {...props} path={path} size={size / 1.5 / 14} />;
-};
-
-export default memo(Icon);
--- gatsby-theme-replica/src/components/global/PageWrapper.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const PageWrapper = styled.div`
-  width: 100%;
-  max-width: 980px;
-  margin: 0 auto;
-`;
-
-export default PageWrapper;
