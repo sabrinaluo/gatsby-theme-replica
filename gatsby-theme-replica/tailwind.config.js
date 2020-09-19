9da6d36d168ae31259dd030161528c351e240499
Sabrina Luo <luohaojing@gmail.com>
2020-09-20 04:42:35 +0800

    refactor: organize css files

--- gatsby-theme-replica/tailwind.config.js
+++ gatsby-theme-replica/tailwind.config.js
@@ -2,13 +2,23 @@ const path = require('path');
 
 const colors = {
   gray: {
+    50: '#fafbfc',
     100: '#f6f8fa',
     light: '#e1e4e8',
+    150: '#c6cbd1',
+    200: '#959da5',
+    300: '#6a737d',
+    medium: '#767676',
     main: '#586069',
     dark: '#24292e',
   },
   orange: '#f9826c',
-  blue: '#0366d6',
+  blue: {
+    100: '#f1f8ff',
+    150: '#def',
+    light: '#c8e1ff',
+    default: '#0366d6',
+  },
   hotpink: '#FF69B4',
 };
 
@@ -24,7 +34,12 @@ module.exports = {
   theme: {
     extend: {
       colors,
+      borderColor: {
+        ...colors,
+        default: colors.gray.light,
+      },
       spacing: {
+        14: '3.5rem',
         '2px': '2px',
         '60px': '60px',
         '74px': '74px',
