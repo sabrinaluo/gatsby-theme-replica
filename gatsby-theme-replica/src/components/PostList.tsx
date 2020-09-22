289a5f7b46ffc90462f2a1ed22b3a6887b461d92
sabrinaluo <luohaojing@gmail.com>
2020-09-22 15:55:49 +0800

    feat: archive post list

---
+++ gatsby-theme-replica/src/components/PostList.tsx
@@ -0,0 +1,44 @@
+import { Link } from 'gatsby';
+import React, { FC } from 'react';
+import { FiStar } from 'react-icons/fi';
+
+import Tag from './Tag';
+
+interface Props {
+  posts: PostNode[];
+}
+
+const PostList: FC<Props> = ({ posts }) => {
+  return (
+    <ul>
+      {posts?.map((post) => (
+        <li key={post.id} className={`py-6 border-b flex`}>
+          <div className='w-10/12'>
+            <h3 className='mb-1 font-semibold text-lg'>
+              <Link to={post.fields.slug}>{post.frontmatter.title} </Link>
+            </h3>
+            <p className='my-2 text-sm text-gray-main'>{post.excerpt}</p>
+            <div className={`py-1`}>
+              {post.frontmatter?.tags?.map(
+                (tag) => tag && <Tag tag={tag} key={tag} />
+              )}
+            </div>
+            <div className='text-xs text-gray-main'>
+              Updated <time>{post.frontmatter.date}</time>
+            </div>
+          </div>
+          <div className='w-2/12 flex items-center justify-end'>
+            <div
+              className={`text-xs px-3 py-1 border bg-gray-100 rounded-md font-semibold flex items-center`}
+            >
+              <FiStar size={16} />
+              <span className={`pl-1`}>star</span>
+            </div>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default PostList;
--- gatsby-theme-replica/src/templates/archive.tsx
+++ gatsby-theme-replica/src/templates/archive.tsx
@@ -1,9 +1,8 @@
-import { Link, graphql } from 'gatsby';
+import { graphql } from 'gatsby';
 import React, { FC } from 'react';
-import { FiStar } from 'react-icons/fi';
 
 import Layout, { LayoutMode } from '../components/Layout';
-import Tag from '../components/Tag';
+import PostList from '../components/PostList';
 
 export const query = graphql`
   query PostListQuery {
@@ -40,34 +39,7 @@ const ArchiveTemplate: FC<Props> = ({ data }) => {
   const posts = data.allMdx.nodes;
   return (
     <Layout mode={LayoutMode.NavTab}>
-      <ul>
-        {posts?.map((post) => (
-          <li key={post.id} className={`py-6 border-b flex`}>
-            <div className='w-10/12'>
-              <h3 className='mb-1 font-semibold text-lg'>
-                <Link to={post.fields.slug}>{post.frontmatter.title} </Link>
-              </h3>
-              <p className='my-2 text-sm text-gray-main'>{post.excerpt}</p>
-              <div className={`py-1`}>
-                {post.frontmatter?.tags?.map(
-                  (tag) => tag && <Tag tag={tag} key={tag} />
-                )}
-              </div>
-              <div className='text-xs text-gray-main'>
-                Updated <time>{post.frontmatter.date}</time>
-              </div>
-            </div>
-            <div className='w-2/12 flex items-center'>
-              <div
-                className={`text-xs px-3 py-1 border bg-gray-100 rounded-md font-semibold flex items-center`}
-              >
-                <FiStar size={16} />
-                <span className={`pl-1`}>star</span>
-              </div>
-            </div>
-          </li>
-        ))}
-      </ul>
+      <PostList posts={posts} />
     </Layout>
   );
 };
