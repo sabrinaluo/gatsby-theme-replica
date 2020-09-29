ece7dbb7c3d30c7c2c52e4fbda9a238dc79934ca
Sabrina Luo <luohaojing@gmail.com>
2020-09-29 23:26:34 +0800

    refactor: extract rss config

---
+++ gatsby-theme-replica/_gatsby/config/rss.js
@@ -0,0 +1,62 @@
+const query = `
+{
+  site {
+    siteMetadata {
+      title
+      description
+      siteUrl
+      site_url: siteUrl
+    }
+  }
+}
+        `;
+
+const feedQuery = `
+{
+  allMdx(
+    filter: { slug: { ne: "README" } },
+    sort: { order: DESC, fields: [frontmatter___date] },
+  ) {
+    edges {
+      node {
+        excerpt
+        html
+        fields { slug }
+        frontmatter {
+          title
+          date
+        }
+      }
+    }
+  }
+}
+            `;
+
+const feeds = [
+  {
+    serialize: ({ query: { site, allMdx } }) => {
+      return allMdx.edges.map((edge) => {
+        return Object.assign({}, edge.node.frontmatter, {
+          description: edge.node.excerpt,
+          date: edge.node.frontmatter.date,
+          url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+          guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+          custom_elements: [{ 'content:encoded': edge.node.html }],
+        });
+      });
+    },
+    query: feedQuery,
+    output: '/rss.xml',
+    title: `RSS Feed`,
+    // optional configuration to insert feed reference in pages:
+    // if `string` is used, it will be used to create RegExp and then test if pathname of
+    // current page satisfied this regular expression;
+    // if not provided or `undefined`, all pages will have feed reference inserted
+    match: '^/post/',
+  },
+];
+
+module.exports = {
+  query,
+  feeds,
+};
--- gatsby-theme-replica/gatsby-config.js
+++ gatsby-theme-replica/gatsby-config.js
@@ -1,6 +1,7 @@
 const DEFAULT_CONTENT_PATH = 'content';
 require('ts-node').register({ files: true });
 const tailwindConfig = require('./tailwind.config');
+const rssFeedOption = require('./_gatsby/config/rss');
 
 module.exports = ({ contentPath = DEFAULT_CONTENT_PATH }) => ({
   siteMetadata: {
@@ -12,62 +13,7 @@ module.exports = ({ contentPath = DEFAULT_CONTENT_PATH }) => ({
     'gatsby-plugin-react-helmet',
     {
       resolve: 'gatsby-plugin-feed',
-      options: {
-        query: `
-          {
-            site {
-              siteMetadata {
-                title
-                description
-                siteUrl
-                site_url: siteUrl
-              }
-            }
-          }
-        `,
-        feeds: [
-          {
-            serialize: ({ query: { site, allMdx } }) => {
-              return allMdx.edges.map((edge) => {
-                return Object.assign({}, edge.node.frontmatter, {
-                  description: edge.node.excerpt,
-                  date: edge.node.frontmatter.date,
-                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                  custom_elements: [{ 'content:encoded': edge.node.html }],
-                });
-              });
-            },
-            query: `
-              {
-                allMdx(
-                  filter: { slug: { ne: "README" } },
-                  sort: { order: DESC, fields: [frontmatter___date] },
-                ) {
-                  edges {
-                    node {
-                      excerpt
-                      html
-                      fields { slug }
-                      frontmatter {
-                        title
-                        date
-                      }
-                    }
-                  }
-                }
-              }
-            `,
-            output: '/rss.xml',
-            title: `RSS Feed`,
-            // optional configuration to insert feed reference in pages:
-            // if `string` is used, it will be used to create RegExp and then test if pathname of
-            // current page satisfied this regular expression;
-            // if not provided or `undefined`, all pages will have feed reference inserted
-            match: '^/post/',
-          },
-        ],
-      },
+      options: rssFeedOption,
     },
     {
       resolve: 'gatsby-source-filesystem',
